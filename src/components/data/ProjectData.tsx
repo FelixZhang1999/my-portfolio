cdc02cb105fa1fc2e9c4149f65d6560de05171ac
Felix Zhang <53685644+FelixZhang1999@users.noreply.github.com>
2024-02-05 11:29:41 -0800

    Add Project

--- src/components/data/ProjectData.tsx
+++ src/components/data/ProjectData.tsx
@@ -1,8 +1,17 @@
 export const ProjectData=[
     {
         id:1,
-        title:"AI Job Hunt Website",
-        about: "AI Job Hunt is an innovative web platform revolutionizing job search. Users effortlessly enter their resumes, empowering the AI-driven system to scour the web for tailored employment opportunities. Built with React and Spring Boot, with seamless deployment on AWS.",
+        title:"HerbCaring Website",
+        about: "HerbCaring is a herbal product startup. I developed a static website to enhance the company's online presence. The frontend uses React and Nextjs and the website is hosted on Cloudflare.",
+        tags:["React", "Cloudflare"],
+        demo:"https://herbcaring.com/",
+        github:"https://github.com/FelixZhang1999/HerbCareWebpage",
+        image: "",
+    },
+    {
+        id:2,
+        title:"AI Job Hunt Website (Demo Backend Service Stopped)",
+        about: "AI Job Hunt is an innovative web platform revolutionizing job search. Users enter their resumes, empowering the AI-driven system to scrape the web for suitable employment opportunities. Built with React and Spring Boot, with deployment on AWS.",
         tags:["React", "AWS", "Spring Boot"],
         demo:"https://aijobhunt.xyz/",
         github:"https://github.com/FelixZhang1999/AIJobHuntClient",
