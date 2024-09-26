df31a917341e659e1bd841dff70026840a4cb9c2
Felix Zhang <53685644+FelixZhang1999@users.noreply.github.com>
2024-09-26 11:52:26 -0700

    add aicreationhub

--- src/components/data/ProjectData.tsx
+++ src/components/data/ProjectData.tsx
@@ -1,6 +1,15 @@
 export const ProjectData=[
     {
         id:1,
+        title:"AICreationhub Website (In progress)",
+        about: "AICreationhub is an online social platform that allows users to share and discover AI-generated content. The website is built with React and Nextjs and the backend is built with Spring Boot and deployed on AWS. The website is still in development.",
+        tags:["React", "Cloudflare", "AWS", "Spring Boot"],
+        demo:"https://aicreationhub.xyz/",
+        github:"https://github.com/FelixZhang1999/",
+        image: "",
+    },
+    {
+        id:2,
         title:"HerbCaring Website",
         about: "HerbCaring is a herbal product startup. I developed a static website to enhance the company's online presence. The frontend uses React and Nextjs and the website is hosted on Cloudflare.",
         tags:["React", "Cloudflare"],
@@ -9,7 +18,7 @@ export const ProjectData=[
         image: "",
     },
     {
-        id:2,
+        id:3,
         title:"AI Job Hunt Website (Demo Backend Service Stopped)",
         about: "AI Job Hunt is an innovative web platform revolutionizing job search. Users enter their resumes, empowering the AI-driven system to scrape the web for suitable employment opportunities. Built with React and Spring Boot, with deployment on AWS.",
         tags:["React", "AWS", "Spring Boot"],
